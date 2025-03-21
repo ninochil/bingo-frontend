afeaf53b88c125cf0351928c1d963c5575874b5b
jinjin <sakura0419gen@icloud.com>
2025-03-21 16:54:37 +0900

    [update] tailwindでもcolor componentが使えるように

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,54 +1,50 @@
 // テーマを管理するためのファイル
 
-export const colors = {
-  orange: '#FFA142',
-  red: '#FF6E61',
-  yellow: '#FFD84D',
-  green: '#A1D9B2',
-  blue: '#6D9DC5',
-  gray: '#4C4747',
-  white: '#FEFEFE',
-  light_gray: '#77828B'
-};
-
-export const font = {
-  banana: `'YDWBananaSlipPlus', sans-serif`
-}
-
-export const fontSizes = {
-  xs: '16px',
-  sm: '20px',
-  base: '28px',
-  lg: '64px',
-  xl: '120px'
-};
-
-// 角の丸み : theme.borderRadius.〇〇で使用できる
-export const borderRadius = {
-  xs: '10px',
-  sm: '15px',
-  md: '30px',
-  lg: '40px'
-}
-
-export const border = {
-  xs: {
-    width: '3px', 
-    style: 'solid'
+const theme = {
+  // color : theme.colors.〇〇で使用できる
+  colors: {
+    orange: '#FFA142',
+    red: '#FF6E61',
+    yellow: '#FFD84D',
+    green: '#A1D9B2',
+    blue: '#6D9DC5',
+    gray: '#4C4747',
+    white: '#FEFEFE',
+    light_gray: '#77828B'
   },
-  sm: {
-    width: '5px', 
-    style: 'solid'
-  }
-}
 
-// styled-components 用
-export const theme = {
-  colors,
-  font,
-  fontSizes,
-  borderRadius,
-  border
+  // fontSize : theme.fontSize.〇〇で使用できる
+  fontSizes: {
+    xs: '16px',
+    sm: '20px',
+    base: '28px',
+    lg: '64px',
+    xl: '120px'
+  },
+
+  font: {
+    banana: `'YDWBananaSlipPlus', sans-serif`,
+  },
+
+  // 角の丸み : theme.borderRadius.〇〇で使用できる
+  borderRadius: {
+    xs: '10px',
+    sm: '15px',
+    md: '30px',
+    lg: '40px'
+  },
+
+  // 線の太さ : theme.border.〇〇で使用できる
+  border: {
+    xs: {
+      width: '3px', 
+      style: 'solid'
+    },
+    sm: {
+      width: '5px', 
+      style: 'solid'
+    }
+  },
 };
 
-export type ThemeType = typeof theme;
+export default theme;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,51 @@ export default {
   },
   plugins: [],
 };
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: '#FFA142',
+        red: '#FF6E61',
+        yellow: '#FFD84D',
+        green: '#A1D9B2',
+        blue: '#6D9DC5',
+        gray: '#4C4747',
+        white: '#FEFEFE',
+        light_gray: '#77828B'
+      },
+      fontFamily: {
+        banana: ['YDWBananaSlipPlus', 'sans-serif'],
+      },
+      borderRadius: {
+        xs: '10px',
+        sm: '15px',
+        md: '30px',
+        lg: '40px'
+      },
+      fontSizes: {
+        xs: '16px',
+        sm: '20px',
+        base: '28px',
+        lg: '64px',
+        xl: '120px'
+      },    
+      border: {
+        xs: {
+          width: '3px', 
+          style: 'solid'
+        },
+        sm: {
+          width: '5px', 
+          style: 'solid'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
