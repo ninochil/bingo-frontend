aee4c1d1dcbf7e25d48b7e622323b1c718deaa44
jinjin <sakura0419gen@icloud.com>
2025-03-21 13:26:53 +0900

    [update] tailwindでも使えるように変更

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,48 +1,54 @@
 // テーマを管理するためのファイル
 
-const theme = {
-  // color : theme.colors.〇〇で使用できる
-  colors: {
-    orange: '#FFA142',
-    red: '#FF6E61',
-    yellow: '#FFD84D',
-    green: '#A1D9B2',
-    blue: '#6D9DC5',
-    gray: '#4C4747',
-    white: '#FEFEFE',
-    light_gray: '#77828B'
-  },
-
-  // fontSize : theme.fontSize.〇〇で使用できる
-  fontSizes: {
-    xs: '16px',
-    sm: '20px',
-    base: '28px',
-    lg: '64px',
-    xl: '120px'
-  },
-
-  font: `'YDWBananaSlipPlus', sans-serif`,
-
-  // 角の丸み : theme.borderRadius.〇〇で使用できる
-  borderRadius: {
-    xs: '10px',
-    sm: '15px',
-    md: '30px',
-    lg: '40px'
-  },
-
-  // 線の太さ : theme.border.〇〇で使用できる
-  border: {
-    xs: {
-      width: '3px', 
-      style: 'solid'
-    },
-    sm: {
-      width: '5px', 
-      style: 'solid'
-    }
-  },
+export const colors = {
+  orange: '#FFA142',
+  red: '#FF6E61',
+  yellow: '#FFD84D',
+  green: '#A1D9B2',
+  blue: '#6D9DC5',
+  gray: '#4C4747',
+  white: '#FEFEFE',
+  light_gray: '#77828B'
 };
 
-export default theme;
+export const font = {
+  banana: `'YDWBananaSlipPlus', sans-serif`
+}
+
+export const fontSizes = {
+  xs: '16px',
+  sm: '20px',
+  base: '28px',
+  lg: '64px',
+  xl: '120px'
+};
+
+// 角の丸み : theme.borderRadius.〇〇で使用できる
+export const borderRadius = {
+  xs: '10px',
+  sm: '15px',
+  md: '30px',
+  lg: '40px'
+}
+
+export const border = {
+  xs: {
+    width: '3px', 
+    style: 'solid'
+  },
+  sm: {
+    width: '5px', 
+    style: 'solid'
+  }
+}
+
+// styled-components 用
+export const theme = {
+  colors,
+  font,
+  fontSizes,
+  borderRadius,
+  border
+};
+
+export type ThemeType = typeof theme;
