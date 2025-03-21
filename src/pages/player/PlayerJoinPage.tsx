f22360f60065ddd887033ef73acfdeb4a793cb62
jinjin <sakura0419gen@icloud.com>
2025-03-22 03:28:20 +0900

    [update] PlayerJoinPage デザイン完成

--- src/pages/player/PlayerJoinPage.tsx
+++ src/pages/player/PlayerJoinPage.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import { TextField } from "@mui/material";
-
-const TextFieldStyle = {
-  width: "200px",
-  
-};
+import React, { useState } from 'react';
+import TextField from '../../components/TextField';
+import NewButton from '../../components/NewButton';
 
 const PlayerJoinPage: React.FC = () => {
+  // useStateの追加
+  const [name, setName] = useState('');
+
   return (
     <div className='bg-green h-screen flex justify-center items-center'>
-      <div className='h-[448px] w-[330px] bg-white rounded-md flex flex-col items-center'>
-        <div className='h-10 mt-10 font-banana text-sm'>参加コード</div>
-        <div className='flex flex-col justify-center'>
-          <TextField 
-            label="Input" 
-            style={TextFieldStyle}
-          /> 
+      <div className='h-[448px] w-[330px] bg-white rounded-md flex flex-col justify-center items-center'>
+        <div className="flex flex-col items-center">
+          <TextField
+              label="参加コード"
+              placeholder="主催者に確認してね！"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+              label="ニックネーム"
+              placeholder="みんながわかる名前にしてね！"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+          />
+          <div className='flex flex-col items-center gap-3 pt-4'>
+            <NewButton 
+                label="すすむ"
+                to="./TopPage.tsx"
+                direction="forward"
+            />
+            <NewButton 
+              label="もどる"
+              to="./TopPage.tsx"
+              direction="backward"
+            />
+          </div>
         </div>
         
       </div>
