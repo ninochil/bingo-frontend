07096c320aa366857ff23c8bd83c4754cb10ab09
jinjin <sakura0419gen@icloud.com>
2025-03-22 11:11:38 +0900

    [update] ボタンの無効有効判定追加

--- src/components/NewButton.tsx
+++ src/components/NewButton.tsx
@@ -6,25 +6,39 @@ type NewButtonProps = {
   to?: string; // 遷移先のパスを指定
   onClick?: () => void; 
   className?: string;   
+  disabled?: boolean;
   // 進むボタンか戻るボタンか
   direction: "forward" | "backward";
 }
 
-const NewButton: React.FC<NewButtonProps> = ({ label, to, onClick, className = '', direction }) => {
-  const buttonClass = direction === "forward"
-    ? "bg-blue hover:bg-blue text-white" // 進むボタンの色 (青)
-    : "bg-lightgray hover:bg-lightgray text-white"; // 戻るボタンの色 (灰色)
-
+const NewButton: React.FC<NewButtonProps> = ({ 
+  label,
+  to, 
+  onClick, 
+  className = '', 
+  direction, 
+  disabled = false
+}) => {
   const navigate = useNavigate();
+
+  // ボタンの色
+  const buttonClass = disabled
+    ? "bg-lightgray hover:bg-lightgray text-white opacity-50 cursor-not-allowed" // 無効時
+    : direction === "forward"
+      ? "bg-blue hover:bg-blue text-white" // 進むボタンの色 (青)
+      : "bg-gray hover:bg-lightgray text-white"; // 戻るボタンの色 (灰色)
   
-  const handleClick = () => {
-    if (onClick) onClick();
-    if (to) navigate(to);
-  };
+    const handleClick = () => {
+      if (disabled) return; // 無効なら動かない
+      if (onClick) onClick();
+      if (to) navigate(to);
+    };
+    
 
   return (
     <button
       onClick={handleClick}
+      disabled={disabled}
       className={`w-[175px] h-[48px] text-sm rounded-sm border-xs border-white text-white focus:outline-none shadow-custom ${buttonClass} ${className}`}
     >
       {label}
