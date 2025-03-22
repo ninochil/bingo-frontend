132de4f88245251bdd98f6631f13dd435cdb0697
AluFu <0hp3261131m102u@ezweb.ne.jp>
2025-03-23 01:43:39 +0900

    [update]5秒後に遷移するようにした

--- src/pages/player/PlayerStandbyPage.tsx
+++ src/pages/player/PlayerStandbyPage.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import NewButton from "../../components/NewButton";
 
 const PlayerStandbyPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  // 10秒後に自動的にビンゴ画面に遷移
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/PlayerBingoPage");
+    }, 5000); // 10秒 (10000ミリ秒)
+
+    return () => clearTimeout(timer); // クリーンアップ
+  }, [navigate]);
+
   return (
     <div className="bg-green h-screen flex flex-col justify-center items-center gap-6">
-      {/* TODO: 0人固定にしちゃっててすまにー */}
-      <div className="text-base text-gray text-outline">待機リスト  0人</div>
+      <div className="text-base text-gray text-outline">待機リスト 0人</div>
       <div className="h-[406px] w-[318px] bg-white rounded-md border-sm border-orange">
-        {/* TODO: ここにリスト入れるにだ */}
+        {/* TODO: ここにリストを入れる */}
       </div>
       <div className="text-sm text-gray text-outline">ゲーム開始までしばらくお待ちください</div>
-      <NewButton 
-        label="キャンセル"
-        to="/"
-        direction="backward"
-      />
+      <NewButton label="キャンセル" to="/" direction="backward" />
     </div>
   );
-}
+};
 
 export default PlayerStandbyPage;
