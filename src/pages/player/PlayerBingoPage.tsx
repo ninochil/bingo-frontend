8c5e3d9e96d7c3bb04c2c6f475b0b6051228c30a
jinjin <sakura0419gen@icloud.com>
2025-03-23 13:41:59 +0900

    [update] 投票する人のポップアップ作成

--- src/pages/player/PlayerBingoPage.tsx
+++ src/pages/player/PlayerBingoPage.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
+import NewButton from "../../components/NewButton";
+import RadioButton from "../../components/RadioButton";
 
 const WS_URL = "ws://localhost:5002/ws/game";
 
@@ -6,6 +8,17 @@ const PlayerBingoPage: React.FC = () => {
   const socketRef = useRef<WebSocket | null>(null);
   const [questions, setQuestions] = useState<string[]>([]);
   const [markedQuestions, setMarkedQuestions] = useState<string[]>([]);
+
+  // popup
+  const [isOpen, setIsOpen] = useState(false);
+  const openPopup = () => setIsOpen(true);
+  const closePopup = () => setIsOpen(false);
+
+  // ラジオボタンで誰が選択されてるか
+  const [selectedOption, setSelectedOption] = useState('option1');
+  const handleRadioChange = (value: string) => {
+    setSelectedOption(value);
+  };
 
   useEffect(() => {
     const socket = new WebSocket(WS_URL);
@@ -41,13 +54,13 @@ const PlayerBingoPage: React.FC = () => {
   return (
     <div className="bg-green-200 flex h-screen items-center justify-center">
       <div className="text-center">
-        <h1 className="mb-10 text-4xl font-bold">あなたのビンゴカード</h1>
-        <div className="inline-block rounded-lg bg-white p-4 shadow-lg">
+        <h1 className="mb-10 text-base">あなたのビンゴカード</h1>
+        <div className="inline-block rounded-lg bg-white p-4 shadow-lg mb-5">
           <div className="grid grid-cols-3 gap-4">
             {questions.map((text, index) => (
               <div
                 key={index}
-                className={`flex h-32 w-32 items-center justify-center rounded-md border-4 p-2 text-center text-sm transition-all duration-500 ${
+                className={`flex h-[97px] w-[97px] items-center justify-center rounded-xs border-4 border-yellow p-2 text-center text-gray text-sm transition-all duration-500 ${
                   markedQuestions.includes(text)
                     ? "border-yellow bg-yellow"
                     : "border-yellow-400 bg-white"
@@ -62,7 +75,48 @@ const PlayerBingoPage: React.FC = () => {
             ))}
           </div>
         </div>
+        <NewButton 
+          label="ポップアップを開く"
+          direction="forward"
+          onClick={openPopup}
+        />
       </div>
+
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white rounded-md p-8 w-[300px] text-center shadow-lg relative">
+            <div className="text-sm mb-5 text-gray">良かった人を投票してね！</div>
+            {/* TODO: ここに回答者リストの名前表示 */}
+            <div className="flex flex-col mx-3 gap-2 mb-6">
+              <RadioButton
+                label="もーすぎ"
+                value="option1"
+                checked={selectedOption === 'option1'}
+                onChange={handleRadioChange}
+              />
+              <RadioButton
+                label="かまの"
+                value="option2"
+                checked={selectedOption === 'option2'}
+                onChange={handleRadioChange}
+              />
+              <RadioButton
+                label="じん"
+                value="option3"
+                checked={selectedOption === 'option3'}
+                onChange={handleRadioChange}
+              />
+            </div>
+
+            <NewButton
+              label="投票する"
+              direction="forward"
+              onClick={closePopup}
+            />
+          </div>
+        </div>
+      )}
+
     </div>
   );
 };
